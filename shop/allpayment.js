08c008004679704cf975275a32fa65862c8ffeb1
Tommygans <91420132+Tommygans@users.noreply.github.com>
2021-11-22 14:44:12 +0700

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,25 +1,17 @@
 const allpayment = (prefix) => {
-return `PAYMENT BY ZEEONE OFC
+return `PAYMENT BY TOMMY OFC
 
 *🏧 BANK*
 
 1. BRI 
 	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
 	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
-2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ 0857 8058 9674
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ 0857 8058 9674
 	
 Semuanya atas nama RI****DI
 
